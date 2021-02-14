fba1d63e1ea30380b7e2c7844a6bfa4e0659cc50
sarah-fuchs <78563120+sarah-fuchs@users.noreply.github.com>
2021-02-14 01:58:57 +0100

    Added country code

--- src/app.js
+++ src/app.js
@@ -63,6 +63,9 @@ function displayTemperature(response) {
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML=response.data.name;
 
+    let countryElement = document.querySelector("#country");
+    countryElement.innerHTML= response.data.sys.country;
+
     let descriptionElement = document.querySelector("#description");
     descriptionElement.innerHTML= response.data.weather[0].description;
 
