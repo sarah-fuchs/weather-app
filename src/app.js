414083aeee5581cf8a5783af36fa651843a820a8
sarah-fuchs <78563120+sarah-fuchs@users.noreply.github.com>
2021-02-14 01:46:14 +0100

    Implemented 3-hour forecast

--- src/app.js
+++ src/app.js
@@ -37,8 +37,23 @@
       ];
       let month = months[now.getMonth()];
 
+// Hours for Forecasts
+function formatHours(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    return `${hours}:${minutes}`;
 
+}
 // Search Form
+
 function displayTemperature(response) {
     console.log(response.data.main.temp);
     let temperatureElement = document.querySelector("#temperature")
@@ -68,12 +83,35 @@ function displayTemperature(response) {
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt",response.data.weather[0].description);
 }
+function displayForecast(response) {
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = null;
+    let forecast = null;
+    
+    for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+ 
+    forecastElement.innerHTML  += `
+    <div class="col-2">
+    <h3>
+    ${formatHours(forecast.dt * 1000)}
+    </h3>
+    <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" />
+    <div class="weather-forecast-temperature">
+    <strong>${Math.round(forecast.main.temp_max)}°</strong> ${Math.round(forecast.main.temp_min)}°    
+    </div>
+    </div> 
+    `
+}
+}
 
 function search(city) {
     let apiKey="11c6b1943d69dd9ab2b79eb46ab8283b";
     let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
     axios.get(apiUrl).then(displayTemperature);
 
+    apiUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
